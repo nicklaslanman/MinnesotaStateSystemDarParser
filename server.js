c2d3efa4ea79971c9ffb3441790607cfab708cb5
nicklaslanman <39254482+nicklaslanman@users.noreply.github.com>
2018-11-28 18:40:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -32,24 +32,42 @@ pdf(dataBuffer).then(function(data) {
   console.log("------------------------------------------------------------------------------");
   
   // DARs parser
-  var patternFall = /[F]\s[0-9][0-9]\s[A-Z]{2,5}..[0-9]{2,3}..................([A-z]).*/g;
-  var patternSpring = /[S][0-9][0-9]\s\s[A-Z]{2,5}..[0-9]{2,3}..................([A-z]).*/g;
-  var patternSummer = /[S][S][0-9][0-9]\s[A-Z]{2,5}..[0-9]{2,3}..................([A-z]).*/g;
-  var posFall = data.text.match(patternFall);
-  var posSpring = data.text.match(patternSpring);
-  var posSummer = data.text.match(patternSummer);
+  // Tech ID
+  var techIdPattern = /[:]\d\d\d\d\d\d\d\d\d\d/;
+  var techId = data.text.match(techIdPattern);
+  techId[0] = techId[0].substr(3);
+  console.log("Tech ID: " + techId);
+  // Student Name
+  var namePattern = /.*\n[P][R][O]/;
+  var name = data.text.match(namePattern);
+  name[0] = name[0].slice(0, -4);
+  console.log("Student Name: " + name);
+  // Advisor Name
+  var advisorPattern = /[A][d][v][i][s][o][r][:].*/;
+  var advisor = data.text.match(advisorPattern);
+  advisor[0] = advisor[0].substr(9);
+  console.log("Advisor: " + advisor);
+  // Cumulative GPA
+  var cumGpaPattern = /...................................................[0-9]... [G][P][A]/;
+  var cumGpa = data.text.match(cumGpaPattern);
+  cumGpa[0] = cumGpa[0].substr(51);
+  cumGpa[0] = cumGpa[0].slice(0, -3);
+  console.log("Cumulative GPA: " + cumGpa);
+  // Major GPA
+  var majorGpaPattern = /[:]............................................[0-9].../;
+  var majorGpa = data.text.match(majorGpaPattern);
+  majorGpa[0] = majorGpa[0].substr(45);
+  console.log("Major GPA: " + majorGpa);
+  // Courses
+  var coursePattern = /[A-Z].[0-9]..[A-Z]{2,5}..[0-9]{2,3}...........[^WF][^WF][^WF].*/g;
+  var courses = data.text.match(coursePattern);
   var academicHistory = [];
-  for (var i=0; i<posFall.length; i++){
-    posFall[i] = posFall[i].substr(5);
-    academicHistory.push(posFall[i]);
+  for (var i=0; i<courses.length; i++){
+    courses[i] = courses[i].substr(5);
+    academicHistory.push(courses[i]);
   }
-  for (var i=0; i<posSpring.length; i++){
-    posSpring[i] = posSpring[i].substr(5);
-    academicHistory.push(posSpring[i]);
-  }
-  for (var i=0; i<posSummer.length; i++){
-    posSummer[i] = posSummer[i].substr(5);
-    academicHistory.push(posSummer[i]);
-  }
-  console.log(removeDuplicates(academicHistory));
+  console.log("Academic History:");
+  console.log("   Course ID:    Grade:     Course Name:");
+  academicHistory = removeDuplicates(academicHistory);
+  console.log(academicHistory);
 });
